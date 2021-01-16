3cb20b15ff7a1fbc2885da4306cba27c97a2a836
Dionicio Hernandez <dhernan528@gmail.com>
2021-01-16 10:15:09 -0500

    Modified pybank main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,27 @@
+# Establish Import Statement
+import os
 
+# Module for reading files
+import csv
+
+# Establish path to file 
+csvpath = os.path.join("..", "Resources", "budget_data.csv")
+
+# Open the CSV
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    for row in csvreader:
+        months = row[0]
+        profit_loss = row[1]
+
+# Extract total number of months in dataset
+    for row in csvreader:
+            months.append(row[0])
+
+# Add profit and loss values
+profit_loss.append(int(row[1]))
+
+# Variables
+total_number_months = len(months)
+profit_loss_net_total = sum((profit_loss))
