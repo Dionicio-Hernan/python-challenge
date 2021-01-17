fc05849d01713ace8405b2944edc1a61967b5459
Dionicio Hernandez <dhernan528@gmail.com>
2021-01-17 15:33:34 -0500

    fourth update

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,8 +5,7 @@ import os
 import csv
 
 # Establish variables
-total_months = 0
-current _pl = 0 
+total_months = []
 previous_pl = 0
 net_pl = 0 
 total_change_pl = 0 
@@ -23,7 +22,7 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
 
-    print(csv_reader)
+    print(csvreader)
     
 # Read header row first
     csv_header = next(csvreader)
@@ -31,19 +30,35 @@ with open(csvpath) as csvfile:
 
 # Read each row of data after the header
     for row in csvreader:
-        print(row[0])
 
-# Get total number of months from this file
-total_months = row(0)
+    # Get total number of months from this file
+        total_months = len(set(row[0]))
 
-# Calculate net total amount in profit/loss
-highs = []
-high = int(row [1])
-highs.append(high)
+    # Calculate net total amount in profit/loss
+        current_pl = int(row[1])
+        net_pl += current_pl
 
 # Changes in Profit Loss over time, find average of changes
+previous_pl = current_pl
+next
+else:
+    current_change_pl = current_pl - previous_pl
+    total_change_pl += current_change_pl
+    previous_pl = current_pl
+
+# Biggest increase in profits
+if current_change_pl > max_increase:
+    biggest_increase = current_change_pl
+    biggest_decrease = row(0)
+
+# Biggest decrease in profits
+elif current_change_pl < biggest_decrease
+biggest_decrease = current_change_pl
+biggest_decrease_month = row (0)
+
+# Average change in profit and loss
+avg_change_pl = total_change_pl/total_months
 
 # Print outputs
-print (total_months)
-print (net_profit_loss) 
-print (highs)
+print ("Total months:" + total_months)
+print (net_pl) 
